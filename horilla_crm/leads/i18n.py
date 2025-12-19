9d8bb715b7fddcfa072e9f03ae396041b1a4df7d
Horilla <support@horilla.com>
2025-12-19 18:26:03 +0530

    [UPDT] LOCALE: Added locale in all apps and added msgstr of ar, de and fr and also added trans and getext

---
+++ horilla_crm/leads/i18n.py
@@ -0,0 +1,8 @@
+from django.utils.translation import gettext_lazy as _
+
+_("CRM")
+_("CRM module for managing leads, contacts, and opportunities.")
+_("Horilla CRM")
+_("Welcome to Horilla CRM")
+_("Please sign up to access Horilla CRM")
+_("Horilla CRM")
